e0f391194e6a2a52c2d1a1250e7a54a1da28a34a
MD Tareq Mahmud <tareqmahmud9@gmail.com>
2021-09-04 17:58:27 +0600

    Solve reverse string

---
+++ 344.reverse-string.ts
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=344 lang=typescript
+ *
+ * [344] Reverse String
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify s in-place instead.
+ */
+function reverseString(s: string[]): void {
+    s.reverse();
+};
+// @lc code=end
+
