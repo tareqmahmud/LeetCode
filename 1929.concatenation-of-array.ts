9464e23c958677396058990baf85f2daa57cfcbe
MD Tareq Mahmud <tareqmahmud9@gmail.com>
2021-09-04 15:27:13 +0600

    Solve concatenation of array

---
+++ 1929.concatenation-of-array.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=1929 lang=typescript
+ *
+ * [1929] Concatenation of Array
+ */
+
+// @lc code=start
+function getConcatenation(nums: number[]): number[] {
+    return [...nums, ...nums];
+};
+// @lc code=end
+
